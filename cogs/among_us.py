c05dac2409b29b00b47c124ffc795a906f2ea26f
Lars <larsbeijaard@outlook.com>
2020-10-29 22:45:42 +0100

    A claim command which allows only that user to mute and unmute everyone

--- cogs/among_us.py
+++ cogs/among_us.py
@@ -1,14 +1,30 @@
 from discord.ext import commands
+import discord
 
 class AmongUs(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self.party_leader = discord.Member
 
-    @commands.command(name="ping")
-    async def ping(self, ctx):
-        await ctx.send('Pong!')
+    @commands.command(name='claim')
+    async def claim(self, ctx):
+        try:
+            embed = discord.Embed(
+                description=f'{self.party_leader.mention}, you are the **Party Leader** in '
+                            f'`{self.party_leader.voice.channel.name}`!',
+                color=discord.Color.from_rgb(0, 125, 0))
+            can_be_party_leader = True
+        except Exception:
+            embed = discord.Embed(
+                description=f'{self.party_leader.mention}, please join a voice-channel to be able claiming **Party Leader**.',
+                color=discord.Color.from_rgb(125, 0, 0))
+            can_be_party_leader = False
 
+        if can_be_party_leader:
+            self.party_leader = ctx.author
+
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(AmongUs(client))
