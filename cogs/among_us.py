c698d9611415fbe2a2751cb7653e14e287d88e4e
Lars <larsbeijaard@outlook.com>
2020-10-29 22:50:33 +0100

    Added comments

--- cogs/among_us.py
+++ cogs/among_us.py
@@ -9,18 +9,19 @@ class AmongUs(commands.Cog):
 
     @commands.command(name='claim')
     async def claim(self, ctx):
-        try:
+        try:  # Try assigning a member as party leader
             embed = discord.Embed(
                 description=f'{self.party_leader.mention}, you are the **Party Leader** in '
                             f'`{self.party_leader.voice.channel.name}`!',
                 color=discord.Color.from_rgb(0, 125, 0))
             can_be_party_leader = True
-        except Exception:
+        except Exception: # Failed to assign the member as party leader
             embed = discord.Embed(
                 description=f'{self.party_leader.mention}, please join a voice-channel to be able claiming **Party Leader**.',
                 color=discord.Color.from_rgb(125, 0, 0))
             can_be_party_leader = False
 
+        # Assign the user as party leader if possible
         if can_be_party_leader:
             self.party_leader = ctx.author
 
