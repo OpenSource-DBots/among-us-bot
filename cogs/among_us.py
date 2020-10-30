b8f88e194fb841484fea66220ffd6d7731dd9e2d
Lars <larsbeijaard@outlook.com>
2020-10-30 14:55:11 +0100

    Set the party leader if user is in voice channel

--- cogs/among_us.py
+++ cogs/among_us.py
@@ -11,19 +11,21 @@ class AmongUs(commands.Cog):
     async def claim(self, ctx):
         try:  # Try assigning a member as party leader
             embed = discord.Embed(
-                description=f'{self.party_leader.mention}, you are the **Party Leader** in '
-                            f'`{self.party_leader.voice.channel.name}`!',
+                description=f'{ctx.author.mention}, you are the **Party Leader** in '
+                            f'`{ctx.author.voice.channel.name}`!',
                 color=discord.Color.from_rgb(0, 125, 0))
             can_be_party_leader = True
         except Exception:  # Failed to assign the member as party leader
             embed = discord.Embed(
-                description=f'{self.party_leader.mention}, please join a voice-channel to be able claiming **Party Leader**.',
+                description=f'{ctx.author.mention}, please join a voice-channel to be able claiming **Party Leader**.',
                 color=discord.Color.from_rgb(125, 0, 0))
             can_be_party_leader = False
 
         # Assign the user as party leader if possible
         if can_be_party_leader:
             self.party_leader = ctx.author
+        else:
+            self.party_leader = discord.Member
 
         await ctx.send(embed=embed)
 
