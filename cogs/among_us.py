1c140eb6922ecce924c1a42afec92c2c1e2da2ce
Lars <larsbeijaard@outlook.com>
2020-11-01 19:52:03 +0100

    You can claim the party leader and it auto. removed the party leader when leave

--- cogs/among_us.py
+++ cogs/among_us.py
@@ -24,24 +24,36 @@ class AmongUs(commands.Cog):
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         if member == self.party_leader:
-            for user in self.get_users_in_channel(member.voice.channel.id):
-                if member != user:
-                    await user.edit(mute=self.party_leader.voice.self_mute)
+            # Make sure the user is still in the channel
+            try:
+                for user in self.get_users_in_channel(member.voice.channel.id):
+                    if member != user:
+                        await user.edit(mute=self.party_leader.voice.self_mute)
+            except Exception:
+                self.party_leader = None
 
     @commands.command(name='claim')
     async def claim(self, ctx):
-        # Make sure no one else can claim party leader if there is already one
-        if self.party_leader is not None:
-            await send_discord_embed(
-                ctx=ctx,
-                description=f'{ctx.author.mention}, **Party Leader** has already claimed by '
-                            f'{self.party_leader.mention}',
-                color=discord.Color.from_rgb(125, 0, 0))
-
-            return
-
         # Try assigning a user as party leader
         try:
+            # Notify the user when its trying to claim party leader, but already is
+            if self.party_leader == ctx.author:
+                await send_discord_embed(
+                    ctx=ctx,
+                    description=f'{ctx.author.mention}, you are already a **Party Leader** of `{ctx.author.voice.channel.name}`',
+                    color=discord.Color.from_rgb(125, 0, 0))
+
+                return
+            # Make sure no one else can claim party leader if there is already one
+            elif self.party_leader is not None:
+                await send_discord_embed(
+                    ctx=ctx,
+                    description=f'{ctx.author.mention}, **Party Leader** has already claimed by '
+                                f'{self.party_leader.mention}',
+                    color=discord.Color.from_rgb(125, 0, 0))
+
+                return
+
             await send_discord_embed(
                 ctx=ctx,
                 description=f'{ctx.author.mention}, you are the **Party Leader** in '
@@ -51,10 +63,11 @@ class AmongUs(commands.Cog):
             can_be_party_leader = True
 
         # Failed to assign the user as party leader
-        except discord.DiscordException:
+        except Exception:
             await send_discord_embed(
                 ctx=ctx,
-                description=f'{ctx.author.mention}, please join a voice-channel to be able claiming **Party Leader**.',
+                description=f'{ctx.author.mention}, please join a voice-channel to be able claiming **Party Leader**.'
+                            f'If you are, there went something wrong.',
                 color=discord.Color.from_rgb(125, 0, 0))
 
             can_be_party_leader = False
