6d8a7bf99add247d0bd4d31558492c6685a499b7
Lars <larsbeijaard@outlook.com>
2020-11-01 17:38:28 +0100

    Added comments and removed DRY code

--- cogs/among_us.py
+++ cogs/among_us.py
@@ -1,33 +1,25 @@
 from discord.ext import commands
 import discord
 
+
+"""
+Summary:
+    Send a Discord embedded message
+Params:
+    ctx: context message
+    description: The embed its description
+    color: The color of the embed
+"""
+async def send_discord_embed(ctx, description: str, color: discord.Color):
+    embed = discord.Embed(description=description, color=color)
+    await ctx.send(embed=embed)
+
+
 class AmongUs(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        self.party_leader = discord.Member
-
-    @commands.command(name='claim')
-    async def claim(self, ctx):
-        try:  # Try assigning a member as party leader
-            embed = discord.Embed(
-                description=f'{ctx.author.mention}, you are the **Party Leader** in '
-                            f'`{ctx.author.voice.channel.name}`!',
-                color=discord.Color.from_rgb(0, 125, 0))
-            can_be_party_leader = True
-        except Exception:  # Failed to assign the member as party leader
-            embed = discord.Embed(
-                description=f'{ctx.author.mention}, please join a voice-channel to be able claiming **Party Leader**.',
-                color=discord.Color.from_rgb(125, 0, 0))
-            can_be_party_leader = False
-
-        # Assign the user as party leader if possible
-        if can_be_party_leader:
-            self.party_leader = ctx.author
-        else:
-            self.party_leader = discord.Member
-
-        await ctx.send(embed=embed)
+        self.party_leader = None
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -36,9 +28,53 @@ class AmongUs(commands.Cog):
                 if member != user:
                     await user.edit(mute=self.party_leader.voice.self_mute)
 
+    @commands.command(name='claim')
+    async def claim(self, ctx):
+        # Make sure no one else can claim party leader if there is already one
+        if self.party_leader is not None:
+            await send_discord_embed(
+                ctx=ctx,
+                description=f'{ctx.author.mention}, **Party Leader** has already claimed by '
+                            f'{self.party_leader.mention}',
+                color=discord.Color.from_rgb(125, 0, 0))
+
+            return
+
+        # Try assigning a user as party leader
+        try:
+            await send_discord_embed(
+                ctx=ctx,
+                description=f'{ctx.author.mention}, you are the **Party Leader** in '
+                            f'`{ctx.author.voice.channel.name}`!',
+                color=discord.Color.from_rgb(0, 125, 0))
+
+            can_be_party_leader = True
+
+        # Failed to assign the user as party leader
+        except discord.DiscordException:
+            await send_discord_embed(
+                ctx=ctx,
+                description=f'{ctx.author.mention}, please join a voice-channel to be able claiming **Party Leader**.',
+                color=discord.Color.from_rgb(125, 0, 0))
+
+            can_be_party_leader = False
+
+        # Assign the user as party leader if possible
+        if can_be_party_leader:
+            self.party_leader = ctx.author
+        else:
+            self.party_leader = discord.Member
+
+    """
+    Summary:
+        Get all the users in the same channel als the party leader
+    Returns:
+        List of all users in the same channel als the party leader
+    """
     def get_users_in_channel(self, channel_id) -> list:
         members = self.client.get_channel(channel_id).members
         return members
+
 
 def setup(client):
     client.add_cog(AmongUs(client))
