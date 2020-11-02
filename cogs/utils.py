40e84b57f409edb5940e98883d6d4418e6d4980b
Lars <larsbeijaard@outlook.com>
2020-11-02 23:06:19 +0100

    Added the correct (custom) prefix in the help command

--- cogs/utils.py
+++ cogs/utils.py
@@ -10,21 +10,22 @@ class Utils(commands.Cog):
 
     @commands.command(name='help', aliases=['h', 'info'])
     async def help(self, ctx):
-        embed = discord.Embed(title='Among Us Bot Information', color=discord.Color.from_rgb(0, 125, 0))
+        embed = discord.Embed(title='Among Us Bot Information', color=discord.Color.from_rgb(0, 255, 0))
         embed.set_thumbnail(url=self.client.user.avatar_url)
         embed.add_field(name='Standard Commands',
-                        value='.claim\n'
-                              '.unclaim',
+                        value=f':small_blue_diamond:{self.client.command_prefix}claim\n'
+                              f':small_blue_diamond:{self.client.command_prefix}unclaim',
                         inline=True)
         embed.add_field(name='Advance Commands',
-                        value='.prefix',
+                        value=f':small_blue_diamond:{self.client.command_prefix}prefix',
                         inline=True)
         embed.add_field(name='How does this bot work?',
-                        value='You have to join a voice channel in order to `.claim` it. When you have claimed the '
-                              'voice channel - whenever you mute yourself, the other will be muted as well. And '
-                              'whenever you unmute yourself, the other will be unmuted as well. If you do not want to '
-                              'be a party leader anymore, simply type `.unclaim`.\n'
-                              'If you want to see this message again type `.help`.',
+                        value=f'You have to join a voice channel in order to `{self.client.command_prefix}claim` it. '
+                              f'When you have claimed the voice channel - whenever you mute yourself, the other will '
+                              f'be muted as well. And whenever you unmute yourself, the other will be unmuted as well. '
+                              f'If you do not want to be a party leader anymore, simply type '
+                              f'`{self.client.command_prefix}unclaim`.\n'
+                              f'If you want to see this message again type `{self.client.command_prefix}help`.',
                         inline=False)
         embed.add_field(name='Invite Bot',
                         value=f'[Link](https://discord.com/api/oauth2/authorize?'
@@ -40,10 +41,32 @@ class Utils(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command(name='prefix')
-    async def set_prefix(self, ctx, *, prefix):
+    @commands.has_permissions(administrator=True)
+    async def set_prefix(self, ctx, *, prefix=None):
+        if prefix is not None:
+            try:
+                self.client.set_prefix(prefix=prefix)
+
+                # Update the prefix in the bot presence
+                await self.client.change_presence(
+                    status=discord.Status.online,
+                    activity=discord.Activity(type=discord.ActivityType.watching,
+                                              name=f'out for imposters | {self.client.command_prefix}help'))
+
+                embed = discord.Embed(
+                    description=f'The prefix has been changed to `{prefix}`',
+                    color=discord.Color.from_rgb(0, 255, 0)
+                )
+                await ctx.send(embed=embed)
+
+                return
+
+            except Exception:
+                pass
+
         embed = discord.Embed(
-            description='This command has not been created yet.',
-            color=discord.Color.from_rgb(125, 0, 0)
+            description=f'Please use the valid format: `{self.client.command_prefix}prefix <prefix>`',
+            color=discord.Color.from_rgb(255, 0, 0)
         )
         await ctx.send(embed=embed)
 
